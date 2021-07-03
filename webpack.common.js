bf4fe76def51d01e8b50db72f103779906809916
IhorP <eldhim@gmail.com>
2021-07-03 21:31:11 +0300

    build(app): do initial step

---
+++ webpack.common.js
@@ -0,0 +1,118 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+module.exports = {
+  plugins: [
+    new WebpackCleanupPlugin({
+      exclude: ['.git/**/*', 'web.config', 'README.md', 'favicon.ico']
+    }),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /uk|ru|en-gb/),
+    new HtmlWebpackPlugin({
+      title: 'Paintus',
+      brand: 'Paintus',
+      // color: process.env.BRAND_COLOR,
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets/favicons',
+          to: 'assets/favicons'
+        },
+        {
+          from: path.resolve(__dirname, './src/site.webmanifest'),
+          to: path.resolve(__dirname, 'build'),
+        },
+        {
+          from: path.resolve(__dirname, './src/browserconfig.xml'),
+          to: path.resolve(__dirname, 'build'),
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      Root: path.resolve('./'),
+      Source: path.resolve('./src/'),
+      Assets: path.resolve('./src/assets'),
+      Components: path.resolve('./src/components'),
+      Containers: path.resolve('./src/containers'),
+      Configs: path.resolve('./src/configs'),
+      Utils: path.resolve('./src/utils'),
+      Colors: path.resolve('./src/configs/colors'),
+      Constants: path.resolve('./src/configs/constants'),
+      TranslationsRoot: path.resolve('./src/translations')
+    }
+  },
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
+        test: /\.js$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/img/[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/fonts/[name].[ext]'
+            }
+          }
+        ]
+      },
+    ]
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,99 @@
+const { merge } = require('webpack-merge');
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
+
+const path = require('path');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [
+    new ZipPlugin({
+      // OPTIONAL: defaults to the Webpack output path (above)
+      // can be relative (to Webpack output path) or absolute
+      path: 'zip',
+
+      // OPTIONAL: defaults to the Webpack output filename (above) or,
+      // if not present, the basename of the path
+      filename: 'my_app.zip',
+
+      // OPTIONAL: defaults to 'zip'
+      // the file extension to use instead of 'zip'
+      extension: 'ext',
+
+      // OPTIONAL: defaults to the empty string
+      // the prefix for the files included in the zip file
+      pathPrefix: 'relative/path',
+
+      // OPTIONAL: defaults to the identity function
+      // a function mapping asset paths to new paths
+      pathMapper: (assetPath) => {
+        // put all pngs in an `images` subdir
+        if (assetPath.endsWith('.png')) return path.join(path.dirname(assetPath), 'images', path.basename(assetPath));
+        return assetPath;
+      },
+
+      // OPTIONAL: defaults to including everything
+      // can be a string, a RegExp, or an array of strings and RegExps
+      include: [/\.js$/],
+
+      // OPTIONAL: defaults to excluding nothing
+      // can be a string, a RegExp, or an array of strings and RegExps
+      // if a file matches both include and exclude, exclude takes precedence
+      exclude: [/\.png$/, /\.html$/],
+
+      // yazl Options
+
+      // OPTIONAL: see https://github.com/thejoshwolfe/yazl#addfilerealpath-metadatapath-options
+      fileOptions: {
+        mtime: new Date(),
+        mode: 0o100664,
+        compress: true,
+        forceZip64Format: false,
+      },
+
+      // OPTIONAL: see https://github.com/thejoshwolfe/yazl#endoptions-finalsizecallback
+      zipOptions: {
+        forceZip64Format: false,
+      },
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: '[hash].bundle.min.js'
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // gets a name, i.e. node_modules/packageName/not/this/part.js or node_modules/packageName
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            // avoids potential troubles concerned with servers:
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+
+      },
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: true,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: false,
+      })
+    ],
+  }
+});
