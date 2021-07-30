c056abfd9f3bf314f1b9f67d998e29dda619025b
IhorP <eldhim@gmail.com>
2021-07-30 21:53:59 +0300

    build: add eslint

--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <main>
-        <div/>
+        <div />
         <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
