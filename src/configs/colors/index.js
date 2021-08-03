3b753b70ce1ff9db61f200740b1e03bfe2310ceb
IhorP <eldhim@gmail.com>
2021-08-03 23:08:53 +0300

    refactor: change fonts, add color constants

---
+++ src/configs/colors/index.js
@@ -0,0 +1,12 @@
+export default {
+  primary: {
+    _white: '#ffffff',
+
+    _border_color: 'rgba(255, 255, 255, 0.125)',
+
+    _section_bg: '#4c5c96',
+    _header_bg: 'rgba(53, 56, 73, 0.95)',
+
+  },
+
+};
