53ef054d691bf0ab8be893ad40d09871464910ce
IhorP <eldhim@gmail.com>
2021-08-08 19:29:18 +0300

    feat: add spinner component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export Footer from './Footer';
 export SimpleScrollDialog from './SimpleScrollDialog';
 export ScrollTop from './ScrollTop';
 export Backdrop from './Backdrop';
+export Spinner from './Spinner';
